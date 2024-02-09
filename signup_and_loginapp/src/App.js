3d5e554867b5b29db747d1f46dc290c254be28f2
Khushi Ranjan <ikhushijuly19@gmail.com>
2024-02-09 17:37:58 +0530

    updated

--- signup_and_loginapp/src/App.js
+++ signup_and_loginapp/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Profile from "./components/profile/profile";
-import Login from "./components/login/login";
-import Register from "./components/register/register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Profile from './components/profile/profile';
+import Login from './components/login/login';
+import Register from './components/register/register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 
 function App() {
@@ -12,6 +12,9 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          {/* Add a route for the root path */}
+          <Route path="/" element={<Login setLoginUser={setLoginUser} />} />
+
           <Route path="/profile" element={user && user._id ? <Profile user={user} setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} />} />
           <Route path="/login" element={<Login setLoginUser={setLoginUser} />} />
           <Route path="/register" element={<Register />} />
